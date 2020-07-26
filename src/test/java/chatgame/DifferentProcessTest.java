6631edd45f22ccd1823fc1748ce9a35fe16f6e1c
Garip Tipici <gariptipici@gmail.com>
2020-07-26 21:04:41 +0300

    tests added

--- src/test/java/chatgame/DifferentProcessTest.java
+++ src/test/java/chatgame/DifferentProcessTest.java
@@ -1,5 +1,29 @@
 package chatgame;
 
+
+import org.junit.Before;
+
+import task.multiprocesschatgame.data.Player;
+import task.multiprocesschatgame.service.ChatService;
+
 public class DifferentProcessTest {
+	
+	Player initiator;
+	Player receiver;
+	ChatService chatRoom;
+	String message;
+	
+	@Before
+	public void init() {
+		initiator = new Player("initiator", message, true);
+		receiver = new Player("receiver", message, false);
+		chatRoom = new ChatService();
+		initiator.registerToChatRoom(chatRoom);
+        receiver.registerToChatRoom(chatRoom);
+        message = "hey";
+        
+	}
+	
 
 }
+

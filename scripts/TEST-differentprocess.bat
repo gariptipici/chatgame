f3cda9f650f59a0e10d9b838d81b8c4d355ec3fb
Garip Tipici <gariptipici@gmail.com>
2020-07-26 19:44:48 +0300

    scripts added

---
+++ scripts/TEST-differentprocess.bat
@@ -0,0 +1,3 @@
+java -jar ../target/chatgame-0.0.1-SNAPSHOT.jar differentprocess initiator receiver hey
+
+PAUSE
---
+++ scripts/TEST-sameprocess.bat
@@ -0,0 +1,3 @@
+java -jar ../target/chatgame-0.0.1-SNAPSHOT.jar sameprocess initiator receiver hey
+
+PAUSE
